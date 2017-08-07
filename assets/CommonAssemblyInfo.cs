c627cd0dee7d4f169a8f7bdd89994a5e389203c4
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-08-08 08:07:50 +1000

    Revert to fixed versioning scheme used by build

--- assets/CommonAssemblyInfo.cs
+++ assets/CommonAssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.0.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+// These "special" version numbers are found and replaced at build time.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.1.1")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
